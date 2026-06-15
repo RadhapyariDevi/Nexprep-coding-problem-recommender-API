ee24d139b67049835247778d67cda98162d40461
decipher-code <vishsangule@gmail.com>
2026-06-15 17:33:35 +0530

    Update database.py

--- cf/database.py
+++ cf/database.py
@@ -8,18 +8,15 @@ load_dotenv()
 MONGO_URI = os.getenv("MONGO_URI")
 DB_NAME = os.getenv("DB_NAME", "algo_recommendation_db")
 
-# 2. Establish connection with explicit timeout controls so it doesn't hang
 client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=5000,tlsCAFile=certifi.where())
 db = client[DB_NAME]
 
 problems_collection = db["cf_problems"]
 
-# This forces Python to test the connection immediately right here
 try:
     client.admin.command('ping')
     print("[database] Successfully authenticated and connected to MongoDB Atlas Cloud!")
 except Exception as e:
     print(f"[database CRITICAL] Could not connect to Atlas. Check your password/IP settings. Error: {e}")
 
-# Build index for optimal sorting paths
 problems_collection.create_index("cf_rating")
